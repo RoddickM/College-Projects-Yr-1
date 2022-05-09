61d105cff92b5a637469efde427bc7c910d9db11
RoddickM <98747226+RoddickM@users.noreply.github.com>
2022-05-09 09:25:04 +0100

    Add files via upload

---
+++ One file code/Python-Lab-02.py
@@ -0,0 +1,21 @@
+surname = ["Fulton-Davies", "Smite", "Mujib", "James-Robinson", "Hemmings",
+           "Al-Rashidi", "Assam", "Been", "Hall", "Unsworth", "Dawson", "Street",
+           "Khan Tasawar", "William", "Abunada", "Ali", "Liaqat"]
+print(surname[5])
+
+nicholls_address = {
+    "Campus Name": "Nicholls Campus",
+    "Street Name": "Hyde Road",
+    "Borough": "Ardwick",
+    "City": "Manchester",
+    "Post Code": "M12 6BA"
+}
+print("\n" + str(nicholls_address))
+
+fav_colours = ("Pink", "Purple", "Green", "Purple", "Purple", "Red", "Red", "Purple",
+               "Blue", "Maroon", "Blue", "Black", "Blue", "Blue", "Black", "Black")
+print("\n" + str(fav_colours))
+
+fav_col_set = {"Pink", "Purple", "Green", "Purple", "Purple", "Red", "Red", "Purple",
+               "Blue", "Maroon", "Blue", "Black", "Blue", "Blue", "Black", "Black"}
+print("\n" + str(fav_col_set))
