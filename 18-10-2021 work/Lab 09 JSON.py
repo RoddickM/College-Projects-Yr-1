4960ddedeb9f8990731f9a8a8488ef89b3bde24e
RoddickM <98747226+RoddickM@users.noreply.github.com>
2022-09-07 09:51:27 +0100

    Delete 18-10-2021 work directory

--- 18-10-2021 work/Lab 09 JSON.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-import_data = pd.read_json("dataset02.js")
-
-
-print(import_data.head())
-print(import_data.head(12))
-print(import_data.tail())
-print(import_data.info())
--- 18-10-2021 work/Lab 09.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-import_data = pd.read_csv("dataset01.csv")
-
-
-print(import_data.head())
-print(import_data.head(12))
-print(import_data.tail())
-print(import_data.info())
